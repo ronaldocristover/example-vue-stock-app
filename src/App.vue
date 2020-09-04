c59a3eed9c4f36af155d2f73d6d0d9e8e8aaad4b
Ronaldo Cristover <ronaldochristover@gmail.com>
2020-09-04 18:56:27 +0700

    Init Module

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
+    
     <div class="container">
-
+        <app-header></app-header>
+        <div class="row">
+                <router-view></router-view>
+        </div>
     </div>
 </template>
 
 <script>
-
+    import Header from './components/Header.vue'
     export default {
-
+        components: {
+            appHeader: Header 
+        }
     }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+            </div>
+
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+                    <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</template>
